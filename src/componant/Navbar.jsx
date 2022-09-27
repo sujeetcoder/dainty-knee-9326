1c23d55fd98b113d5278f3be508c6fa51d97bf03
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2022-09-27 21:37:09 +0530

    fD1 final

--- src/componant/Navbar.jsx
+++ src/componant/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
         <>
         <Link to={"/"} >
-        <Image w="4%"margin="auto" marginTop="12px" src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5fc4feea074f5b86a6c62285_freshly-logo-R.svg" alt="image" />
+        <Image w="4%" margin="auto" marginTop="12px" src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5fc4feea074f5b86a6c62285_freshly-logo-R.svg" alt="image" />
         </Link>
         <Flex style={{fontWeight: "500"}} pos="sticky" w="100%" h="70px" top="0px" paddingTop={"17px"} bgColor={"#fffdf7"}  >
             
